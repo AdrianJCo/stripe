970bedaae5b52ddd48c832ea70bbdc6cd19eb9b9
sophiehicks <sophiehicks@192.168.1.247>
2023-08-27 17:36:52 +0100

    second commit

---
+++ heroku-mediaibntaymiyyah-pay/src/main/java/uk/mediaibntaymiyya/stripe/HerokuMediaibntaymiyyahPayApplication.java
@@ -0,0 +1,13 @@
+package uk.mediaibntaymiyya.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuMediaibntaymiyyahPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuMediaibntaymiyyahPayApplication.class, args);
+	}
+
+}
---
+++ heroku-mediaibntaymiyyah-pay/src/test/java/uk/mediaibntaymiyya/stripe/HerokuMediaibntaymiyyahPayApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.mediaibntaymiyya.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuMediaibntaymiyyahPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
